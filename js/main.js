4005297008dcb6d2adec65e5a2c0c61d0c71d1e1
Malorie Kramer <mkramer2089@gmail.com>
2016-08-16 23:28:46 -0400

    thought bubbles

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-(function() {
-    'use strict';
-    angular
-        .module('portfolio', ['ui.router'])
-        .config(function($stateProvider, $urlRouterProvider) {
+// (function() {
+//     'use strict';
+//     angular
+//         .module('portfolio', ['ui.router'])
+//         .config(function($stateProvider, $urlRouterProvider) {
 
-	  	$urlRouterProvider.otherwise("/");
+// 	  	$urlRouterProvider.otherwise("/");
 
-	  	$stateProvider
-	    .state('home', {
-      	url: '/',
-      	views: {
-      		'main': {
-		        templateUrl: '../views/home.html',
-		        controller: 'MainController',
-		        controllerAs: 'controller'
-		      },
-		  },
-	    });
-	});
-})();
+// 	  	$stateProvider
+// 	    .state('home', {
+//       	url: '/',
+//       	views: {
+//       		'main': {
+// 		        templateUrl: '../views/home.html',
+// 		        controller: 'MainController',
+// 		        controllerAs: 'controller'
+// 		      },
+// 		  },
+// 	    });
+// 	});
+// })();
 
 
