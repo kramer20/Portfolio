f60c87dbdbd8f284f43623c2c9dfeb7dd9ca3b02
Malorie Kramer <mkramer2089@gmail.com>
2016-09-06 22:54:13 -0400

    added some things

--- js/controllers/mainControl.js
+++ js/controllers/mainControl.js
@@ -49,58 +49,51 @@
               $('.smallSkills').addClass('animated smallSkillsAnimation');
               $('.medSkills').addClass('animated medSkillsAnimation');
               $('.skillsBubble').addClass('animated skillsBubbleAnimation');
-              // $('#meWork').addClass('animated meWorkAnimation');
-              // $('.topBottom').addClass('animated meWorkFadeAnimation');
+              $('#meSkills').addClass('animated rateSlideAnimation');
+              $('.upDown').addClass('animated fadeRateAnimation');
               $('.technicalList').addClass('animated skillsListAnimation');
               $('.htmlOne').addClass('animated htmlOneAnimation');
               $('.htmlTwo').addClass('animated htmlTwoAnimation');
               $('.htmlThree').addClass('animated htmlThreeAnimation');
               $('.htmlFour').addClass('animated htmlFourAnimation');
               $('.htmlFive').addClass('animated htmlFiveAnimation');
-              // $('.htmlSix').addClass('animated htmlSixAnimation');
-              // $('.htmlSeven').addClass('animated htmlSevenAnimation');
+            
               $('.cssOne').addClass('animated cssOneAnimation');
               $('.cssTwo').addClass('animated cssTwoAnimation');
               $('.cssThree').addClass('animated cssThreeAnimation');
               $('.cssFour').addClass('animated cssFourAnimation');
               $('.cssFive').addClass('animated cssFiveAnimation');
-              // $('.cssSix').addClass('animated cssSixAnimation');
-              // $('.cssSeven').addClass('animated cssSevenAnimation');
+              
               $('.javaOne').addClass('animated javaOneAnimation');
               $('.javaTwo').addClass('animated javaTwoAnimation');
               $('.javaThree').addClass('animated javaThreeAnimation');
               $('.javaFour').addClass('animated javaFourAnimation');
               $('.javaFive').addClass('animated javaFiveAnimation');
-              // $('.javaSix').addClass('animated javaSixAnimation');
-              // $('.javaSeven').addClass('animated javaSevenAnimation');
+             
               $('.jOne').addClass('animated jOneAnimation');
               $('.jTwo').addClass('animated jTwoAnimation');
               $('.jThree').addClass('animated jThreeAnimation');
               $('.jFour').addClass('animated jFourAnimation');
               $('.jFive').addClass('animated jFiveAnimation');
-              // $('.jSix').addClass('animated jSixAnimation');
-              // $('.jSeven').addClass('animated jSevenAnimation');
+             
               $('.angOne').addClass('animated angOneAnimation');
               $('.angTwo').addClass('animated angTwoAnimation');
               $('.angThree').addClass('animated angThreeAnimation');
               $('.angFour').addClass('animated angFourAnimation');
               $('.angFive').addClass('animated angFiveAnimation');
-              // $('.angSix').addClass('animated angSixAnimation');
-              // $('.angSeven').addClass('animated angSevenAnimation');
+             
               $('.photoOne').addClass('animated photoOneAnimation');
               $('.photoTwo').addClass('animated photoTwoAnimation');
               $('.photoThree').addClass('animated photoThreeAnimation');
               $('.photoFour').addClass('animated photoFourAnimation');
               $('.photoFive').addClass('animated photoFiveAnimation');
-              // $('.photoSix').addClass('animated photoSixAnimation');
-              // $('.photoSeven').addClass('animated photoSevenAnimation');
+            
               $('.designOne').addClass('animated designOneAnimation');
               $('.designTwo').addClass('animated designTwoAnimation');
               $('.designThree').addClass('animated designThreeAnimation');
               $('.designFour').addClass('animated designFourAnimation');
               $('.designFive').addClass('animated designFiveAnimation');
-              // $('.designSix').addClass('animated designSixAnimation');
-              // $('.designSeven').addClass('animated designSevenAnimation');
+              
 
 
             // use jQuery to add class of what animation i want to happen
@@ -110,8 +103,34 @@
            
        });
 
+       var waypointFour = new Waypoint({
+          element: document.getElementById('contact'),
+          handler: function(){
+            // alert('hi');
+              $('#contact .contact').css('opacity', 100);
+              // $('.smallWork').addClass('animated smallWorkAnimation');
+              // $('.medWork').addClass('animated medWorkAnimation');
+              // $('.workBubble').addClass('animated workBubbleAnimation');
+              // // $('#meWork').addClass('animated meWorkAnimation');
+              $('.rightLeft').addClass('animated animationFadeTwo');
 
-      
+            // use jQuery to add class of what animation i want to happen
+          },
+          
+          offset:'10%'
+           
+       });
 
+          jQuery(document).ready(function($) {
+           $('.my-slider').unslider();
+          });
+
+          //on click like stays on active nav item
+          $("li.page-scroll > a").click(function(){
+              // console.log("li.page-scroll > a");
+                $("li.page-scroll > a").removeClass("in-active");
+                console.log("li.page-scroll > a");
+                $(vm).addClass("active");
+          });
     });
 })();
