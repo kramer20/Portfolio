3ad1e731342e364ca4e52b46588704a20382f65f
Malorie Kramer <mkramer2089@gmail.com>
2016-09-20 12:44:28 -0400

    take out skills section

--- js/controllers/mainControl.js
+++ js/controllers/mainControl.js
@@ -20,9 +20,26 @@
               $('.medAbout').addClass('animated medAboutAnimation');
               $('.aboutBubble').addClass('animated aboutBubbleAnimation');
               $('#me').addClass('animated meAnimation');
+              $(".navList a").removeClass("active");
+              $(".aboutScroll").addClass("active"); 
        		},
           
-          offset:'10%'
+          // offset:'15%'
+          
+           
+       });
+
+       //remove nav line from work when in intro section
+       var waypointOne = new Waypoint({
+          element: document.getElementById('intro'),
+          handler: function(){
+            // alert('hi');
+            $(".navList a").removeClass("active");
+            $(".introScroll").addClass("active"); 
+              
+          },
+          
+          offset:'-5%'
            
        });
 
@@ -38,72 +55,73 @@
               $('.my-slider').addClass('animated workSliderAnimation');
               $('#meWork').addClass('animated meWorkFadeAnimation');
               $('#meWorkTwo').addClass('animated meAppearAnimation');
-
+              $(".navList a").removeClass("active");
+              $(".workScroll").addClass("active"); 
           },
           
-          offset:'10%'
+          // offset:'5%'
            
        });
 
-       //animation for skills section
-       var waypointThree = new Waypoint({
-          element: document.getElementById('skills'),
-          handler: function(){
-            // alert('hi');
-              $('#skills .skills').css('opacity', 100);
-              $('.smallSkills').addClass('animated smallSkillsAnimation');
-              $('.medSkills').addClass('animated medSkillsAnimation');
-              $('.skillsBubble').addClass('animated skillsBubbleAnimation');
-              $('#meSkills').addClass('animated rateSlideAnimation');
-              $('.upDown').addClass('animated fadeRateAnimation');
-              $('.technicalList').addClass('animated skillsListAnimation');
-              $('.htmlOne').addClass('animated htmlOneAnimation');
-              $('.htmlTwo').addClass('animated htmlTwoAnimation');
-              $('.htmlThree').addClass('animated htmlThreeAnimation');
-              $('.htmlFour').addClass('animated htmlFourAnimation');
-              $('.htmlFive').addClass('animated htmlFiveAnimation');
+       // //animation for skills section
+       // var waypointThree = new Waypoint({
+       //    element: document.getElementById('skills'),
+       //    handler: function(){
+       //      // alert('hi');
+       //        $('#skills .skills').css('opacity', 100);
+       //        $('.smallSkills').addClass('animated smallSkillsAnimation');
+       //        $('.medSkills').addClass('animated medSkillsAnimation');
+       //        $('.skillsBubble').addClass('animated skillsBubbleAnimation');
+       //        $('#meSkills').addClass('animated rateSlideAnimation');
+       //        $('.upDown').addClass('animated fadeRateAnimation');
+       //        $('.technicalList').addClass('animated skillsListAnimation');
+       //        $('.htmlOne').addClass('animated htmlOneAnimation');
+       //        $('.htmlTwo').addClass('animated htmlTwoAnimation');
+       //        $('.htmlThree').addClass('animated htmlThreeAnimation');
+       //        $('.htmlFour').addClass('animated htmlFourAnimation');
+       //        $('.htmlFive').addClass('animated htmlFiveAnimation');
             
-              $('.cssOne').addClass('animated cssOneAnimation');
-              $('.cssTwo').addClass('animated cssTwoAnimation');
-              $('.cssThree').addClass('animated cssThreeAnimation');
-              $('.cssFour').addClass('animated cssFourAnimation');
-              $('.cssFive').addClass('animated cssFiveAnimation');
+       //        $('.cssOne').addClass('animated cssOneAnimation');
+       //        $('.cssTwo').addClass('animated cssTwoAnimation');
+       //        $('.cssThree').addClass('animated cssThreeAnimation');
+       //        $('.cssFour').addClass('animated cssFourAnimation');
+       //        $('.cssFive').addClass('animated cssFiveAnimation');
               
-              $('.javaOne').addClass('animated javaOneAnimation');
-              $('.javaTwo').addClass('animated javaTwoAnimation');
-              $('.javaThree').addClass('animated javaThreeAnimation');
-              $('.javaFour').addClass('animated javaFourAnimation');
-              $('.javaFive').addClass('animated javaFiveAnimation');
+       //        $('.javaOne').addClass('animated javaOneAnimation');
+       //        $('.javaTwo').addClass('animated javaTwoAnimation');
+       //        $('.javaThree').addClass('animated javaThreeAnimation');
+       //        $('.javaFour').addClass('animated javaFourAnimation');
+       //        $('.javaFive').addClass('animated javaFiveAnimation');
              
-              $('.jOne').addClass('animated jOneAnimation');
-              $('.jTwo').addClass('animated jTwoAnimation');
-              $('.jThree').addClass('animated jThreeAnimation');
-              $('.jFour').addClass('animated jFourAnimation');
-              $('.jFive').addClass('animated jFiveAnimation');
+       //        $('.jOne').addClass('animated jOneAnimation');
+       //        $('.jTwo').addClass('animated jTwoAnimation');
+       //        $('.jThree').addClass('animated jThreeAnimation');
+       //        $('.jFour').addClass('animated jFourAnimation');
+       //        $('.jFive').addClass('animated jFiveAnimation');
              
-              $('.angOne').addClass('animated angOneAnimation');
-              $('.angTwo').addClass('animated angTwoAnimation');
-              $('.angThree').addClass('animated angThreeAnimation');
-              $('.angFour').addClass('animated angFourAnimation');
-              $('.angFive').addClass('animated angFiveAnimation');
+       //        $('.angOne').addClass('animated angOneAnimation');
+       //        $('.angTwo').addClass('animated angTwoAnimation');
+       //        $('.angThree').addClass('animated angThreeAnimation');
+       //        $('.angFour').addClass('animated angFourAnimation');
+       //        $('.angFive').addClass('animated angFiveAnimation');
              
-              $('.photoOne').addClass('animated photoOneAnimation');
-              $('.photoTwo').addClass('animated photoTwoAnimation');
-              $('.photoThree').addClass('animated photoThreeAnimation');
-              $('.photoFour').addClass('animated photoFourAnimation');
-              $('.photoFive').addClass('animated photoFiveAnimation');
+       //        $('.photoOne').addClass('animated photoOneAnimation');
+       //        $('.photoTwo').addClass('animated photoTwoAnimation');
+       //        $('.photoThree').addClass('animated photoThreeAnimation');
+       //        $('.photoFour').addClass('animated photoFourAnimation');
+       //        $('.photoFive').addClass('animated photoFiveAnimation');
             
-              $('.designOne').addClass('animated designOneAnimation');
-              $('.designTwo').addClass('animated designTwoAnimation');
-              $('.designThree').addClass('animated designThreeAnimation');
-              $('.designFour').addClass('animated designFourAnimation');
-              $('.designFive').addClass('animated designFiveAnimation');
-    
-          },
+       //        $('.designOne').addClass('animated designOneAnimation');
+       //        $('.designTwo').addClass('animated designTwoAnimation');
+       //        $('.designThree').addClass('animated designThreeAnimation');
+       //        $('.designFour').addClass('animated designFourAnimation');
+       //        $('.designFive').addClass('animated designFiveAnimation');
+                  
+       //    },
           
-          offset:'10%'
+       //    offset:'10%'
            
-       });
+       // });
 
        //animations for contact section
        var waypointFour = new Waypoint({
@@ -117,59 +135,14 @@
               $('#meContact').addClass('animated animationSlideLeft');
               $('.rightLeft').addClass('animated animationFadeTwo');
               $('.contactCol').addClass('animated contactItemsAnimation');
-           
+              $(".navList a").removeClass("active");
+              $(".contactScroll").addClass("active"); 
           },
           
-          offset:'30%'
+          offset:'22%'
            
        });
 
-       //work scroll line
-       var waypointFive = new Waypoint({
-          element: document.getElementById('work'),
-          handler: function(){
-              $('a.workScroll').css('opacity', 100);
-              $('a.workScroll').addClass('scrollLine');     
-          },
-
-          offset:'10%'
-       });
-
-       //about scroll line
-       var waypointSix = new Waypoint({
-          element: document.getElementById('about'),
-          handler: function(){
-              $('a.workScroll').removeClass('scrollLine');
-              $('a.aboutScroll').css('opacity', 100);
-              $('a.aboutScroll').addClass('scrollLineA');     
-          },
-
-          offset:'10%'
-       });
-
-       //skills scroll line
-       var waypointSeven = new Waypoint({
-          element: document.getElementById('skills'),
-          handler: function(){
-              $('a.aboutScroll').removeClass('scrollLineA');
-              $('a.skillsScroll').css('opacity', 100);
-              $('a.skillsScroll').addClass('scrollLineB');     
-          },
-
-          offset:'10%'
-       });
-
-       //contact scroll line
-       var waypointEight = new Waypoint({
-          element: document.getElementById('contact'),
-          handler: function(){
-              $('a.skillsScroll').removeClass('scrollLineB');
-              $('a.contactScroll').css('opacity', 100);
-              $('a.contactScroll').addClass('scrollLineC');     
-          },
-
-          offset:'10%'
-       });
 
           jQuery(document).ready(function($) {
            $('.my-slider').unslider();
